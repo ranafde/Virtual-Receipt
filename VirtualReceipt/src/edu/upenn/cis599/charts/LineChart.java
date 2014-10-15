9b58148e930bb6d368f79d6e5d979ae9d57b7a06
shashank1601 <shashank1601@gmail.com>
2014-10-14 23:08:22 -0400

    Refactored LineChart(SV)

--- VirtualReceipt/src/edu/upenn/cis599/charts/LineChart.java
+++ VirtualReceipt/src/edu/upenn/cis599/charts/LineChart.java
@@ -1,5 +1,5 @@
 /**
- * Yiran Qin
+ * Shashank Vadlamani
  */
 package edu.upenn.cis599.charts;
 
@@ -51,48 +51,54 @@ public class LineChart extends MyChartHelper {
 	 * @return the built intent
 	 */
 
-	/* very long method. need to be modularized */
 	@Override
 	public Intent execute(Context context) {
-
-		ReceiptDbAdapter mDbHelper = new ReceiptDbAdapter(context);
-		mDbHelper.open();
-		/* change names of values, sumList, allList */
-		List<double[]> values = new ArrayList<double[]>();
-		double[] sumList = mDbHelper.retrieveMonthlyPayment(1);
-		double[] allList = mDbHelper.retrieveMonthlyPayment(2);
-		values.add(sumList);
-		values.add(allList);
-
-		/*
-		 * remove the string concatenation and assign it some variable. pass
-		 * that variable to titles
-		 */
-		String[] titles = new String[] {
-				"Year" + Calendar.getInstance().get(Calendar.YEAR)
-						+ " Monthly Spending", "All Time Monthly Spending" };
-		/* change name of x */
-		List<double[]> x = new ArrayList<double[]>();
+		List<double[]> monthlySpendingValues = getMonthlySpendingList(context);
+		String currentYearMonthlySpendingLegendTitle = "Year "
+				+ Calendar.getInstance().get(Calendar.YEAR)
+				+ " Monthly Spending";
+		String allTimeMonthlySpendingLegendTitle = "All Time Monthly Spending";
+		String[] titles = new String[] { currentYearMonthlySpendingLegendTitle,
+				allTimeMonthlySpendingLegendTitle };
+		List<double[]> months = new ArrayList<double[]>();
 		for (int i = 0; i < titles.length; i++) {
-			x.add(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 });
+			months.add(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 });
 		}
-		/* remove Color.rgb and assign it to a variable describing the color */
 		int[] colors = new int[] { Color.rgb(233, 126, 126),
 				Color.rgb(129, 164, 238) };
 		PointStyle[] styles = new PointStyle[] { PointStyle.CIRCLE,
 				PointStyle.TRIANGLE };
-
 		XYMultipleSeriesRenderer renderer = buildRenderer(colors, styles);
+		setXYMultipleSeriesRenderer(renderer);
+		setChartSettings(renderer, "Monthly Spending", "Month", "", 1, 12, 0,
+				(int) (getMaximumMonthlyPayment(context) * 1.1), Color.BLACK,
+				Color.BLACK);
+		Intent intent = ChartFactory.getLineChartIntent(context,
+				buildDataset(titles, months, monthlySpendingValues), renderer,
+				"Monthly Spending");
+		return intent;
+	}
+
+	private ArrayList<double[]> getMonthlySpendingList(Context context) {
+		ReceiptDbAdapter mDbHelper = new ReceiptDbAdapter(context);
+		mDbHelper.open();
+		ArrayList<double[]> result = new ArrayList<double[]>();
+		double[] currentYearMonthlySpendingList = mDbHelper
+				.retrieveMonthlyPayment(1);
+		double[] allTimeMonthlySpendingList = mDbHelper
+				.retrieveMonthlyPayment(2);
+		result.add(currentYearMonthlySpendingList);
+		result.add(allTimeMonthlySpendingList);
+		mDbHelper.close();
+		return result;
+	}
+
+	private void setXYMultipleSeriesRenderer(XYMultipleSeriesRenderer renderer) {
 		int length = renderer.getSeriesRendererCount();
 		for (int i = 0; i < length; i++) {
 			((XYSeriesRenderer) renderer.getSeriesRendererAt(i))
 					.setFillPoints(true);
 		}
-
-		/*
-		 * create an new method that configures the renderer object and returns
-		 * it, instead of doing it in the same method
-		 */
 		renderer.setXLabels(25);
 		renderer.setYLabels(25);
 		renderer.setShowGrid(true);
@@ -102,50 +108,35 @@ public class LineChart extends MyChartHelper {
 		renderer.setLegendTextSize(15);
 		renderer.setLabelsTextSize(10);
 		renderer.setLegendHeight(120);
-		// renderer.setMargins(new int[] { 20, 30, 15, 20 });
-		// mRenderer.setMargins(new int[] { 20, 30, 15, 0 });
-		// renderer.setApplyBackgroundColor(true);
-		// renderer.setBackgroundColor(Color.WHITE);
-		// renderer.setAxisTitleTextSize(16);
-		// renderer.set
-		// renderer.setZoomButtonsVisible(true);
-
 		renderer.setPanLimits(new double[] { 1, 12, 0, 30000 });
 		renderer.setZoomLimits(new double[] { 1, 12, 0, 30000 });
-
+		renderer.setXLabelsAlign(Align.CENTER);
+		renderer.setXLabels(0);
 		String[] month = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
 				"Aug", "Sep", "Oct", "Nov", "Dec" };
 		for (int i = 0; i < month.length; i++) {
-
 			renderer.addTextLabel(i + 1, month[i]);
 		}
-		renderer.setXLabelsAlign(Align.CENTER);
-		renderer.setXLabels(0);
-
 		int rendererCount = renderer.getSeriesRendererCount();
-		/* not sure what this code snippet does. looks like its a junk code */
+		/* code to set the width of the line in line chart */
 		for (int i = 0; i < rendererCount; i++) {
 			XYSeriesRenderer seriesRenderer = (XYSeriesRenderer) renderer
 					.getSeriesRendererAt(i);
 			seriesRenderer.setLineWidth(6f);
 		}
-
-		setChartSettings(renderer, "Monthly Spending", "Month", "", 1, 12, 0,
-				(int) (getMaximumMonthlyPayment(allList) * 1.1), Color.BLACK,
-				Color.BLACK);
-		Intent intent = ChartFactory.getLineChartIntent(context,
-				buildDataset(titles, x, values), renderer, "Monthly Spending");
-
-		mDbHelper.close();
-		return intent;
 	}
 
-	private double getMaximumMonthlyPayment(double[] sumList) {
+	private double getMaximumMonthlyPayment(Context context) {
+		ReceiptDbAdapter mDbHelper = new ReceiptDbAdapter(context);
+		mDbHelper.open();
+		double[] allTimeMonthlySpendingList = mDbHelper
+				.retrieveMonthlyPayment(2);
 		double max = 0.0;
-		for (double sum : sumList) {
+		for (double sum : allTimeMonthlySpendingList) {
 			if (sum > max)
 				max = sum;
 		}
+		mDbHelper.close();
 		return max;
 	}
 }
